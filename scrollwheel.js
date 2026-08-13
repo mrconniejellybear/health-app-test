26225a63bbf031b2878c1c87ad94882a761f3bed
Connor <147426581+mrconniejellybear@users.noreply.github.com>
2026-08-12 22:17:43 -0700

    5223.6345..2445.

--- scrollwheel.js
+++ scrollwheel.js
@@ -106,7 +106,7 @@ function initRotaryWheel() {
 
   wheel.innerHTML = "";
   const total = moodConfig.length;
-  const radius = 132; // Perfect radius alignment for 300px wheel!
+  const radius = 142; // Perfect radius alignment for 300px wheel!
 
   moodConfig.forEach((item, index) => {
     const slotAngle = (index / total) * 360;
